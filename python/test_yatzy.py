cf2acedf3ea21b14f3b3d05870eb92d3563e5c58
andreaduranvivas <andrea.duran@mail.escuelaing.edu.co>
2024-03-08 14:53:05 +0000

    test

--- python/test_yatzy.py
+++ python/test_yatzy.py
@@ -7,4 +7,72 @@ from yatzy import Yatzy
 def test_demo():
         expected = 15
         actual = 15
-        assert expected == actual        
+        assert expected == actual
+
+
+def test_chance_sumAllDice():
+        d1 = 1
+        d2 = 1
+        d3 = 3
+        d4 = 3
+        d5 = 6
+
+        expected =  14
+        actual = Yatzy.chance(d1, d2, d3, d4, d5)
+
+        assert expected == actual
+
+def test_yatzy_sameNumberDice_fiftyPoints():
+        dice = [1, 1, 1, 1, 1]
+
+        expected =  50
+        actual = Yatzy.yatzy(dice)
+
+        assert expected == actual
+
+def test_yatzy_differentNumberDice_zeroPoints():
+        dice = [1, 1, 1, 2, 1]
+
+        expected =  0
+        actual = Yatzy.yatzy(dice)
+
+        assert expected == actual
+
+def test_ones_sumOfOnes():
+        dice = [1, 1, 1, 2, 1]
+
+        expected =  4
+        actual = Yatzy.ones(dice[0], dice[1], dice[2], dice[3], dice[4])
+
+        assert expected == actual
+
+def test_twos_sumOfTwo():
+        dice = [1, 1, 1, 2, 1]
+
+        expected =  2
+        actual = Yatzy.twos(dice[0], dice[1], dice[2], dice[3], dice[4])
+
+        assert expected == actual
+
+def test_threes_sumOfThree():
+        dice = [1, 1, 1, 3, 3]
+
+        expected =  6
+        actual = Yatzy.threes(dice[0], dice[1], dice[2], dice[3], dice[4])
+
+        assert expected == actual
+
+def test_fours_sumOfFour():
+
+        dice = [4,4,4,4,1]
+        expected = 16
+
+        game = Yatzy(dice[0], dice[1], dice[2], dice[3], dice[4])
+
+        assert expected == game.fours()
+
+def 
+        
+
+
+
